c82c2493aef7eda21c985db6c1614112c31b1b65
rotaruteodor <rotaruteodorgabriel@gmail.com>
2022-10-01 15:07:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/musically/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.musically;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeComponents();
+    }
+
+    private void initializeComponents(){
+    }
+}
